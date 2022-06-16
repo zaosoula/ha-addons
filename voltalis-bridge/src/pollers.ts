f720f0fb8a04ce370f50d769e930dab2c8abdcc9
Zao Soula <contact@zaosoula.fr>
2022-06-16 13:23:17 +0200

    fix(voltalis-bridge): run consumption poller every 5 minutes

--- voltalis-bridge/src/pollers.ts
+++ voltalis-bridge/src/pollers.ts
@@ -1,9 +1,8 @@
 import { Voltalis } from "./lib/voltalis"
 import { poller } from "./lib/poller"
-import { AxiosObservable } from "axios-observable"
 
 export const registerPollers = (voltalis: Voltalis) => {
   return {
-    immediateConsumptionInkW: poller('*/5 * * * * *', voltalis.fetchImmediateConsumptionInkW)
+    immediateConsumptionInkW: poller('*/5 * * * *', voltalis.fetchImmediateConsumptionInkW)
   }
 }
