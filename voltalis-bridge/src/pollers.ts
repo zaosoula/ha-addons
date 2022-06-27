3d82324dd3ca4dca6253de50b1b1da94a48e06f9
Zao Soula <contact@zaosoula.fr>
2022-06-27 11:43:04 +0200

    feat(voltalis-bridge): improve data accuracy

--- voltalis-bridge/src/pollers.ts
+++ voltalis-bridge/src/pollers.ts
@@ -3,6 +3,6 @@ import { poller } from "./lib/poller"
 
 export const registerPollers = (voltalis: Voltalis) => {
   return {
-    immediateConsumptionInkW: poller('*/5 * * * *', voltalis.fetchImmediateConsumptionInkW)
+    immediateConsumptionInkW: poller('*/2 * * * *', voltalis.fetchImmediateConsumptionInkW)
   }
 }
